f8f65e017f4d75b8f182e83c6eb30378b939c2f4
Casey <caseylewis54@yahoo.com>
2025-08-13 14:30:59 -0500

    Adding MealListPage and hardcoding options.

---
+++ MealRoulette/Models/Ingredient.cs
@@ -0,0 +1,8 @@
+﻿namespace MealRoulette.Models;
+
+public class Ingredient
+{
+    public string Name { get; set; }
+    public double Amount { get; set; }
+    public string UnitOfMeasurement { get; set; }
+}
---
+++ MealRoulette/Models/Meal.cs
@@ -0,0 +1,8 @@
+﻿namespace MealRoulette.Models;
+
+public class Meal
+{
+    public string Name { get; set; }
+    public List<Ingredient> Ingredients { get; set; } = new();
+    public int DesiredMonthly { get; set; }
+}
