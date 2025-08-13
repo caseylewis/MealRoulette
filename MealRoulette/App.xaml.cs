9d967f818987e6a212935f8f8eefbb1726086175
Casey <caseylewis54@yahoo.com>
2025-08-13 14:05:49 -0500

    Initial commit from new local repo

---
+++ MealRoulette/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MealRoulette
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
